b530b7492b8dc4e61d835b05dd15af7bdafcf0f0
Борзунов Андрей <andry73@yandex.ru>
2026-03-06 16:04:42 +0500

    models

---
+++ lms/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from embed_video.fields import EmbedVideoField
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+
+    preview = models.ImageField(
+        upload_to="lms/previews/",
+        verbose_name="Превью",
+        blank=True,
+        null=True,
+        help_text="Загрузите превью курса",
+    )
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание курса",
+        help_text="Укажите описание курса",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Укажите название урока",
+    )
+
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс",
+        help_text="Укажите учебный курс",
+        blank=True,
+        null=True,
+    )
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание урока",
+        help_text="Укажите описание урока",
+    )
+
+    preview = models.ImageField(
+        upload_to="lms/previews/",
+        verbose_name="Превью",
+        blank=True,
+        null=True,
+        help_text="Загрузите превью урока",
+    )
+
+    video_url = EmbedVideoField()
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
---
+++ users/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="E-mail", help_text="Укажите E-mail"
+    )
+
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+    city = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+        verbose_name="Город проживания",
+        help_text="Укажите родной город",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите свой аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
