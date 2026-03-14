0e6119ad5246715435af63263a8ba8228d0f3911
Борзунов Андрей <andry73@yandex.ru>
2026-03-14 11:32:23 +0500

    Revision

--- lms/serializers.py
+++ lms/serializers.py
@@ -6,7 +6,7 @@ from lms.models import Course, Lesson
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
-        fields = "__All__"
+        fields = "__all__"
 
 
 class LessonDivSerializer(ModelSerializer):
