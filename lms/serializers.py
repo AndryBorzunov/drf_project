12f3b006980cfaf86e8e19ba8ddbcfa9a3dc9969
Борзунов Андрей <andry73@yandex.ru>
2026-03-17 11:45:11 +0500

    validators

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
+from lms.validators import VideoUrlValidator
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [VideoUrlValidator(field="video_url")]
 
 
 class LessonDivSerializer(ModelSerializer):
